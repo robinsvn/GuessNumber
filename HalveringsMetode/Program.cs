25b976ea05d0c627ceadec1fe1a6ea33e5502814
robinsvn <111857815+robinsvn@users.noreply.github.com>
2022-11-21 08:32:16 +0100

    Add files via upload

---
+++ HalveringsMetode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalveringsMetode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int returnValue = random.Next(1, 50);
+
+            int Guess = 0;
+            int GuessNumber = 0;
+
+            Console.WriteLine("Jeg tenker på ett nummer imellom 1 til 50. Gjett hva det er.");
+
+            while (Guess != returnValue)
+            {
+                Guess = Convert.ToInt32(Console.ReadLine());
+
+                if (Guess < returnValue)
+                {
+                    Console.WriteLine("Nummeret er høyere enn " + Guess + ". Prøv på nytt.");
+                    GuessNumber++;
+                }
+                else if (Guess > returnValue)
+                {
+                    Console.WriteLine("Nummeret er lavere enn " + Guess + ". Prøv på nytt.");
+                    GuessNumber++;
+                }
+
+            }
+
+            Console.WriteLine("Riktig! Svaret var : " + returnValue + ". Du gjettet " + GuessNumber + " Antall ganger.");
+            Console.ReadLine();
+        }
+    }
+}
